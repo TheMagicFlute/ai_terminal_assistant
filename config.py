09c2e091d95e94c02b6ceac5b406675d706cee26
mengqin <1413655386@qq.com>
2025-03-21 15:14:59 +0800

    [feat] config

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 import json
 import os
+import sys
+from pathlib import Path
 
 class Config:
     def __init__(self):
-        if not os.path.exists("config.json"):
-            with open("config.json", "w") as f:
+        self.config_path = Path(sys.executable).parent + "/config.json"
+        if not os.path.exists(self.config_path):
+            with open(self.config_path, "w") as f:
                 config = {
                             "base_url": "your_base_url",
                             "api_key": "your_api_key",
@@ -12,22 +15,22 @@ class Config:
                         }
                 f.write(json.dumps(config))
                 print("config.json created. Please fill in the details or use commendline to set them")
-        with open("config.json", "r") as f:
+        with open(self.config_path, "r") as f:
             self.config = json.load(f)
 
     def set_model(self, model):
         self.config["model"] = model
-        with open("config.json", "w") as f:
+        with open(self.config_path, "w") as f:
             json.dump(self.config, f, indent=4)
 
     def set_base_url(self, base_url):
         self.config["base_url"] = base_url
-        with open("config.json", "w") as f:
+        with open(self.config_path, "w") as f:
             json.dump(self.config, f, indent=4)
 
     def set_api_key(self, api_key):
         self.config["api_key"] = api_key
-        with open("config.json", "w") as f:
+        with open(self.config_path, "w") as f:
             json.dump(self.config, f, indent=4)
 
     def get_config(self):
